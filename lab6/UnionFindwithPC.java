a7bcb006921640e06ed06c9d9e1cb06b5ad72989
dingyuhang <dingyh1996@gmail.com>
2019-12-16 13:06:30 +0800

    lab6

---
+++ lab6/UnionFindwithPC.java
@@ -0,0 +1,77 @@
+public class UnionFind {
+    private int[] items;
+    // TODO - Add instance variables?
+
+    /* Creates a UnionFind data structure holding n vertices. Initially, all
+       vertices are in disjoint sets. */
+    public UnionFind(int n) {
+        // TODO
+        items = new int[n];
+        for (int i = 0; i < n; i++) {
+            items[i] = -1;
+        }
+    }
+
+    /* Throws an exception if v1 is not a valid index. */
+    private void validate(int vertex) {
+        // TODO
+        if (vertex<0 || vertex > items.length) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /* Returns the size of the set v1 belongs to. */
+    public int sizeOf(int v1) {
+        // TODO
+        int root = find(v1);
+        return -items[root];
+    }
+
+    /* Returns the parent of v1. If v1 is the root of a tree, returns the
+       negative size of the tree for which v1 is the root. */
+    public int parent(int v1) {
+        // TODO
+        return items[v1];
+    }
+
+    /* Returns true if nodes v1 and v2 are connected. */
+    public boolean connected(int v1, int v2) {
+        // TODO
+        return find(v1) == find(v2);
+    }
+
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
+       elements, and a union-by-size heuristic is used. If the sizes of the sets
+       are equal, tie break by connecting v1's root to v2's root. Unioning a 
+       vertex with itself or vertices that are already connected should not 
+       change the sets but may alter the internal structure of the data. */
+    public void union(int v1, int v2) {
+        // TODO
+        if (connected(v1,v2)) {
+            return;
+        }
+        int root1 = find(v1);
+        int root2 = find(v2);
+
+        if (items[root1] > items[root2]) {
+            items[root2] += items[root1];
+            items[root1] = root2;
+        }
+        else {
+            items[root1] += items[root2];
+            items[root2] = root1;
+        }
+    }
+
+    /* Returns the root of the set V belongs to. Path-compression is employed
+       allowing for fast search-time. */
+    public int find(int vertex) {
+        // TODO
+        int root = vertex;
+        while (items[root] > 0) {
+            root = items[root];
+        }
+        return root;
+    }
+
+}
