b1c9eed4ab18ea213bd8cb3df8b5a670743c7b90
dingyuhang <dingyh1996@gmail.com>
2019-12-19 20:09:44 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,132 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
 
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Percolation {
+    private WeightedQuickUnionUF wqu;
+    private int openNum;
+    private int[][] openGrid;
+    //private Set<Integer> s;
+    //private Set<Integer> p;
+    //private boolean isPercolate;
+    private int n;
+
+    public Percolation(int N) {
+        wqu = new WeightedQuickUnionUF(N * N + 2);
+        openNum = 0;
+        openGrid = new int[N][N];
+        //s = new TreeSet<>();
+        //p = new TreeSet<>();
+        n = N;
+    }
+
+    public void open(int row, int col) {
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        openGrid[row][col] = 1;
+        openNum += 1;
+        int index = row * n + col;
+
+        if (row == 0) {
+            wqu.union(index, n * n);
+        }
+        if (row == n - 1) {
+            wqu.union(index, n * n + 1);
+        }
+        if (row != 0 && isOpen(row - 1, col)){
+            wqu.union(index,index - n);
+        }
+        if (row != n - 1 && isOpen(row + 1, col)) {
+            wqu.union(index,index + n);
+        }
+        if (col != 0 && isOpen(row, col - 1)) {
+            wqu.union(index,index - 1);
+        }
+        if (col != n - 1 && isOpen(row, col + 1)) {
+            wqu.union(index,index + 1);
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        return openGrid[row][col] == 1;
+    }
+    public boolean isFull(int row, int col) {
+        return wqu.connected(row * n + col, n*n);
+        //return s.contains(wqu.find(row * n + col));
+    }
+    public int numberOfOpenSites() {
+        return openNum;
+    }
+    public boolean percolates() {
+        return wqu.connected(n*n,n*n+1);
+        //return isPercolate;
+    }
+    public static void main(String[] args) {
+
+    }
+
+    /*
+        private void uni(int a, int b) {
+        int ra = wqu.find(a);
+        int rb = wqu.find(b);
+
+        if ((s.contains(ra) && p.contains(rb)) || (s.contains(rb) && p.contains(ra))) {
+            isPercolate = true;
+        }
+
+        wqu.union(a,b);
+        int r = wqu.find(a);
+        if (s.contains(ra)) {
+            s.remove(ra);
+            s.add(r);
+        }
+        if (p.contains(ra)) {
+            p.remove(ra);
+            p.add(r);
+        }
+        if (s.contains(rb)) {
+            s.remove(rb);
+            s.add(r);
+        }
+        if (p.contains(rb)) {
+            p.remove(rb);
+            p.add(r);
+        }
+    }
+
+    public void open(int row, int col) {
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        openGrid[row][col] = 1;
+        openNum += 1;
+        int index = row * n + col;
+
+        if (row == 0) {
+            s.add(index);
+        }
+        if (row == n - 1) {
+            p.add(index);
+        }
+
+        if (row != 0 && isOpen(row - 1, col)){
+            uni(index,index - n);
+        }
+        if (row != n - 1 && isOpen(row + 1, col)) {
+            uni(index,index + n);
+        }
+        if (col != 0 && isOpen(row, col - 1)) {
+            uni(index,index - 1);
+        }
+        if (col != n - 1 && isOpen(row, col + 1)) {
+            uni(index,index + 1);
+        }
+    }
+     */
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,62 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class PercolationStats {
+    private double[] accu;
+    private int t;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N < 0 || T < 0) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        t = T;
+        accu = new double[T];
+
+        int[] indexes = new int[N * N];
+        for (int i = 0; i < N * N; i++){
+            indexes[i] = i;
+        }
+
+        int row;
+        int col;
+
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            StdRandom.shuffle(indexes);
+            int step = 0;
+            while (! p.percolates()) {
+                row = indexes[step] / N;
+                col = indexes[step] % N;
+                p.open(row, col);
+                step += 1;
+            }
+            accu[i] = (double) step / (N * N);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(accu);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(accu);
+    }
+
+    public double confidenceLow() {
+        return (mean() - 0.96 * stddev() / Math.sqrt(t));
+    }
+
+    public double confidenceHigh() {
+        return (mean() + 0.96 * stddev() / Math.sqrt(t));
+    }
+
+    public static void main(String[] args) {
+        PercolationStats test = new PercolationStats(100, 10, new PercolationFactory());
+        System.out.println(test.mean());
+    }
 }
